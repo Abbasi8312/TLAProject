06e845a85186aa03bf451a4abb8a06c28a5ed11a
Ali <abbasi8312@gmail.com>
2024-01-29 17:20:33 +0330

    Test 3

--- src/Tests.java
+++ src/Tests.java
@@ -4,6 +4,8 @@ public class Tests {
         System.out.println("--------------------");
         test2();
         System.out.println("--------------------");
+        test3();
+        System.out.println("--------------------");
     }
 
     private static void test1() {
@@ -69,6 +71,34 @@ public class Tests {
             gnfa.addTransition("4", "5", 'a');
             gnfa.addTransition("5", "3", 'a');
             gnfa.addTransition("5", "4", 'b');
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(NFAToRegex.generateRegex(gnfa));
+    }
+    private static void test3() {
+        GNFA gnfa = new GNFA();
+        gnfa.addState("0");
+        gnfa.addState("1");
+        gnfa.addState("2");
+        gnfa.addState("3");
+        gnfa.addAlphabet('a');
+        gnfa.addAlphabet('b');
+        try {
+            gnfa.setStartState("0");
+            gnfa.setFinalStates(new String[]{"3"});
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            gnfa.addTransition("0", "1", 'a');
+            gnfa.addTransition("0", "0", 'b');
+            gnfa.addTransition("1", "1", 'a');
+            gnfa.addTransition("1", "2", 'b');
+            gnfa.addTransition("2", "2", 'a');
+            gnfa.addTransition("2", "3", 'b');
+            gnfa.addTransition("3", "2", 'a');
+            gnfa.addTransition("3", "1", 'b');
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
