da783a1cf8e8916050bb94e5cf62fed4653ef69b
Ali <abbasi8312@gmail.com>
2024-02-01 00:06:20 +0330

    Get inputs from user

--- src/NFAToRegex.java
+++ src/NFAToRegex.java
@@ -1,42 +1,104 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class NFAToRegex {
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         GNFA gnfa = new GNFA();
-        gnfa.addState("0");
-        gnfa.addState("1");
-        gnfa.addState("2");
-        gnfa.addState("3");
-        gnfa.addState("4");
-        gnfa.addState("t");
-        gnfa.addAlphabet('a');
-        gnfa.addAlphabet('b');
-        try {
-            gnfa.setStartState("0");
-            gnfa.setFinalStates(new String[]{"4"});
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        String input;
+        int n = getNumber("Enter the number of states: ");
+        for (int i = 0; i < n; i++) {
+            String state = getString("Enter the name of state " + (i + 1) + ": ");
+            if (state.equals("qs") || state.equals("qf") || state.matches("^\\s*$")) {
+                System.out.println("Invalid state name. qs, qf and \" \" are reserved. Please enter another name.");
+                i--;
+            } else if (gnfa.hasState(state)) {
+                System.out.println("Duplicate state name. Please enter another name.");
+                i--;
+            } else {
+                gnfa.addState(state);
+            }
         }
-        try {
-            gnfa.addTransition("0", "1", 'a');
-            gnfa.addTransition("0", "t", 'b');
-            gnfa.addTransition("t", "t", 'a');
-            gnfa.addTransition("t", "t", 'b');
-            gnfa.addTransition("1", "t", 'a');
-            gnfa.addTransition("1", "2", 'b');
-            gnfa.addTransition("2", "3", 'a');
-            gnfa.addTransition("2", "2", 'b');
-            gnfa.addTransition("3", "3", 'a');
-            gnfa.addTransition("3", "4", 'b');
-            gnfa.addTransition("4", "4", 'a');
-            gnfa.addTransition("4", "4", 'b');
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        int m = getNumber("Enter the number of alphabets: ");
+        for (int i = 0; i < m; i++) {
+            input = getString("Enter the name of alphabet " + (i + 1) + ": ");
+            char alphabet = input.charAt(0);
+            if (!input.matches("^\\S$")) {
+                System.out.println("Invalid input. Please enter a character.");
+                i--;
+            } else if (gnfa.hasAlphabet(alphabet)) {
+                System.out.println("Duplicate alphabet name. Please enter another name.");
+                i--;
+            } else {
+                gnfa.addAlphabet(alphabet);
+            }
+        }
+        while (true) {
+            String start = getString("Enter the name of start state: ");
+            if (!gnfa.hasState(start)) {
+                System.out.println("State not found. Please enter a valid state name.");
+            } else {
+                gnfa.setStartState(start);
+                break;
+            }
+        }
+        int k = getNumber("Enter the number of final states: ");
+        for (int i = 0; i < k; i++) {
+            String finalState = getString("Enter the name of final state " + (i + 1) + ": ");
+            if (!gnfa.hasState(finalState)) {
+                System.out.println("State not found. Please enter a valid state name.");
+                i--;
+            } else {
+                gnfa.addFinalState(finalState);
+            }
+        }
+        int t = getNumber("Enter the number of transitions: ");
+        for (int i = 0; i < t; i++) {
+            input = getString(
+                    "Enter the source state, destination state and alphabet of transition " + (i + 1) + " seperated by space: ");
+            String[] inputs = input.split(" ");
+            if (inputs.length != 3) {
+                System.out.println("Invalid input.");
+                i--;
+                continue;
+            }
+            String source = inputs[0];
+            String destination = inputs[1];
+            char symbol = inputs[2].charAt(0);
+            if (!gnfa.hasState(source) || !gnfa.hasState(destination)) {
+                System.out.println("State not found. Please enter valid state names.");
+                i--;
+            } else if (!inputs[2].matches("^\\S$")) {
+                System.out.println("Invalid Alphabet. Please enter a character.");
+                i--;
+            } else if (!gnfa.hasAlphabet(symbol)) {
+                System.out.println("Alphabet not found. Please enter a valid alphabet name.");
+                i--;
+            } else {
+                gnfa.addTransition(source, destination, symbol);
+            }
         }
         System.out.println(generateRegex(gnfa));
+        scanner.close();
+    }
+
+    private static String getString(String prompt) {
+        String input;
+        System.out.print(prompt);
+        input = scanner.nextLine();
+        return input;
+    }
+
+    private static int getNumber(String prompt) {
+        String input;
+        System.out.print(prompt);
+        input = scanner.nextLine();
+        while (!input.matches("^[0-9]+$")) {
+            System.out.println("Invalid input. Please enter a number.");
+            System.out.print(prompt);
+            input = scanner.nextLine();
+        }
+        return Integer.parseInt(input);
     }
 
     protected static String generateRegex(GNFA originalGNFA) {
@@ -125,12 +187,7 @@ class GNFA {
         }
     }
 
-    public void addTransition(String from, String to, char ch) throws Exception {
-        if (!hasState(from) || !hasState(to)) {
-            throw new StateException();
-        } else if (!alphabet.contains(ch)) {
-            throw new AlphabetException();
-        }
+    public void addTransition(String from, String to, char ch) {
         addTransition(from, to, String.valueOf(ch));
     }
 
@@ -175,22 +232,12 @@ class GNFA {
         return transitions;
     }
 
-    public void setStartState(String startState) throws Exception {
-        if (!hasState(startState)) {
-            throw new StateException();
-        }
+    public void setStartState(String startState) {
         addTransition(this.startState, startState, "");
     }
 
-    public void setFinalStates(String[] finalStates) throws Exception {
-        for (String finalState : finalStates) {
-            if (!hasState(finalState)) {
-                throw new StateException();
-            }
-        }
-        for (String finalState : finalStates) {
-            addTransition(finalState, this.finalState, "");
-        }
+    public void addFinalState(String finalState) {
+        addTransition(finalState, this.finalState, "");
     }
 
     @Override
@@ -204,10 +251,4 @@ class GNFA {
 
     record Transition(String from, String to, String regex) {
     }
-}
-
-class StateException extends Exception {
-}
-
-class AlphabetException extends Exception {
 }
